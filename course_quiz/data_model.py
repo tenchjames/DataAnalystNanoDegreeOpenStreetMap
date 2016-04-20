75fc8bc651194f4f36044fc5c653b41b422fc476
James Tench <tenchjames@gmail.com>
2016-04-19 20:14:52 -0400

    added course quiz files

---
+++ course_quiz/data_model.py
@@ -0,0 +1,23 @@
+
+
+model = {
+    "_id": "2406124091",
+    "visible": "true",
+    "created": {
+        "version": "2",
+        "changeset": "17206049",
+        "timestamp": "2013-08-03T16:43:42Z",
+        "user": "linuxUser16",
+        "uid": "1219059"
+    },
+    "pos": [41.9757030, -87.6921867],
+    "address": {
+        "housenumber": "5157",
+        "postcode": "60625",
+        "street": "North Lincoln Ave"
+    },
+    "amenity": "restaurant",
+    "cuisine": "mexican",
+    "name": "La Cabana De Don Luis",
+    "phone": "1 (773)-27105176"
+}
